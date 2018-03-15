a45ce4ef1e0a03d0b1b04aa176502b30b177f22d
Alan Leite <alanleite@outlook.com>
2018-03-14 22:26:50 -0300

    dev

--- frontend/src/containers/post/container.jsx
+++ frontend/src/containers/post/container.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { createComment } from '../../module/api'
 import Component from './component'
 
+
 class Post extends React.Component {
+
+    state = {
+        newBody: ''
+    }
+
+    comment = (comment) => {
+        createComment({
+            body: this.state.newBody
+        }).then(r => {
+
+        }).catch(err => {
+
+        })
+    }
+
     render() {
         const post = this.props.post
         return <Component {...post}/>
     }
+
 }
 
 export default Post
--- frontend/src/views/main/component.jsx
+++ frontend/src/views/main/component.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { Grid, Label, Item, Header } from 'semantic-ui-react'
 import Post from './../../containers/post'
+
+const LabelsBlock = styled.div`
+  p {
+    padding-top: 25px;
+    font-size: 26px;
+  }
+`
+
+const PostsBlock = styled.div`
+  p {
+    padding-top: 25px;
+    font-size: 26px;
+  }
+`
 
 export default ({
   category,
@@ -11,49 +26,53 @@ export default ({
   loading,
   error
 }) => (
-  <Grid container>
+    <Grid container>
 
-    <Grid.Row>
-      <Grid.Column>
-        <Header content='Categories' />
-        <Label.Group size='large'>
-          <Label
-            color='blue'
-            basic={!!category}
-            as={Link}
-            to={`/`}
-            content='All'
-            onClick={() => {
-              categoryUpdated(null)
-            }} />
-          {categories.map((c, i) => (
-            <Label
-              color='blue'
-              basic={c.path !== category}
-              key={i}
-              as={Link}
-              to={`/${c.path}`}
-              content={c.name}
-              onClick={() => {
-                categoryUpdated(c.path)
-              }} />
-          ))}
-        </Label.Group>
-      </Grid.Column>
-    </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <LabelsBlock>
+            <p>Choose a category</p>
+            <Label.Group size='large'>
+              <Label
+                color='blue'
+                basic={!!category}
+                as={Link}
+                to={`/`}
+                content='all'
+                onClick={() => {
+                  categoryUpdated(null)
+                }} />
+              {categories.map((c, i) => (
+                <Label
+                  color='blue'
+                  basic={c.path !== category}
+                  key={i}
+                  as={Link}
+                  to={`/${c.path}`}
+                  content={c.name}
+                  onClick={() => {
+                    categoryUpdated(c.path)
+                  }} />
+              ))}
+            </Label.Group>
+          </LabelsBlock>
+        </Grid.Column>
+      </Grid.Row>
 
-    <Grid.Row>
-      <Grid.Column>
-        <Header content='Posts' />
-        <Item.Group divided size='large'>
-          {posts.map((p, i) => (
-            <Post
-              key={i}
-              post={p} />
-          ))}
-        </Item.Group>
-      </Grid.Column>
-    </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <PostsBlock>
+            <p>Showing {posts.length} posts from {category || 'all'}</p>
+            <Item.Group divided size='large'>
+              {posts.map((p, i) => (
+                <Post
+                  key={i}
+                  post={p} />
+              ))}
+            </Item.Group>
+          </PostsBlock>
+        </Grid.Column>
+      </Grid.Row>
 
-  </Grid>
-)
+    </Grid>
+  )
